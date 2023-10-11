11e55f568c62152a430e345af608a1dd030c1dc5
kant2589 <edavid.morales@udea.edu.co>
2023-10-11 09:43:28 -0500

    swagger

--- src/main/java/com/example/kanban/config/WebSecurityConfig.java
+++ src/main/java/com/example/kanban/config/WebSecurityConfig.java
@@ -49,29 +49,6 @@ public class WebSecurityConfig {
         // Devolver directamente SecurityFilterChain
         return http.build();
     }
-/*
-        return http.cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer.authenticationEntryPoint(unauthorizedHandler))
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-                            try {
-                                authorizationManagerRequestMatcherRegistry
-                                        .requestMatchers(HttpMethod.POST, POST_AUTH_WHITELIST).permitAll()
-                                        .requestMatchers(HttpMethod.GET, GET_AUTH_WHITELIST).permitAll()
-                                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                                        .anyRequest()
-                                        .authenticated()
-                                        .and()
-                                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-                            } catch (Exception e) {
-                                throw new ResourceNotFoundException(e.getMessage());
-                            }
-                        }
-                )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable).addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .authenticationProvider(daoAuthenticationProvider()).build();
-    }*/
 
     @Bean
     PasswordEncoder passwordEncoder() {
