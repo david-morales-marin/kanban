d3d7fb49657cd1211b95f12696425bbea7795199
kant2589 <edavid.morales@udea.edu.co>
2023-10-19 13:57:14 -0500

    cors

--- src/main/java/com/example/kanban/controllers/UsuarioController.java
+++ src/main/java/com/example/kanban/controllers/UsuarioController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/login")
 @Tag(name = "Usuario controller", description = "Metodo para la autenticacion del usuario")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class UsuarioController {
 
     @Autowired
